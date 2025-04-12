c137973a480a4007ff848b495a2aec69fd5d750f
Donjheggo <christianjheggfer@gmail.com>
2025-04-13 00:02:34 +0800

    fix: hero bg

--- components/landing-page/hero.tsx
+++ components/landing-page/hero.tsx
@@ -8,7 +8,7 @@ import { memo } from "react";
 
 function Hero() {
   return (
-    <div className="min-h-[700px] mt-20 mx-4">
+    <div className="relative min-h-[700px] mt-20 mx-4">
       <GridPattern
         width={30}
         height={30}
@@ -16,10 +16,12 @@ function Hero() {
         y={-1}
         strokeDasharray={"4 2"}
         className={cn(
-          "[mask-image:radial-gradient(600px_circle_at_center,white,transparent)]"
+          "absolute inset-0 z-0",
+          "[mask-image:radial-gradient(600px_circle_at_center,white,transparent)]",
+          "text-gray-200"
         )}
       />
-      <div className="container mx-auto grid grid-cols-1 md:grid-cols-2">
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 z-10 relative">
         <div className="flex items-center justify-center s">
           <div className="space-y-4 lg:space-y-6 text-center md:text-left">
             <h1 className="text-4xl md:text-5xl lg:text-7xl font-bold font-space-grotesk leading-tight">
